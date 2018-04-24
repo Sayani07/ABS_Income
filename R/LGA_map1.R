0f3ac4f166b7ac24242848ce833247aa20d2c118
Sayani07 <Sayani.Gupta@monash.edu>
2018-04-24 22:45:56 +1000

    Progress Update 2 - minor changes in the code

--- R/LGA_map1.R
+++ R/LGA_map1.R
@@ -26,7 +26,7 @@ IncomeData <- readr::read_csv('Data/Income_Data.csv') %>%
     State == 'Tasmania' ~ 'TAS',
     State == 'Northern Territory' ~ 'NT',
     State == 'Australian Capital Territory' ~ 'ACT',
-    TRUE ~ as.character(State))) %>% filter(Weekly_Household_Income =="$8,000 or more"|Household_Composition == "Total Households")
+    TRUE ~ as.character(State))) %>% filter(Weekly_Household_Income =="$4,500-$4,999"|Household_Composition == "Total Households")
 #Might need to change Column names before this step
 
 
@@ -59,57 +59,5 @@ right_join(IncomeData,by=c("LGA_CODE16" = "LGA_2016")) %>%
   right_join(LGA_map)-> LGA_map_2
 
  
-ggplot(LGA_map_2) + geom_polygon(aes(long, lat, group = group), colour = 'grey')
-# 
-
-## Electorates
-
-# electShp <- rgdal::readOGR('elect')
-#electSmall <- rmapshaper::ms_simplify(electShp, keep = 0.05)
-#save(electSmall, file = "electSmall.Rda")
-# load("~/mapsR/electSmall.Rda")
-# 
-# elect_data <- electSmall@data
-# elect_data$id <- row.names(elect_data)
-# elect_map <- ggplot2::fortify(electSmall)
-# elect_map$group <- paste("g",elect_map$group,sep=".")
-# elect_map$piece <- paste("p",elect_map$piece,sep=".")
-# 
-# # Incorporate population
-# elect_data %>%
-#   select(id, Elect_div, State, Area_SqKm, Total_Population) %>%
-#   rename(name =  Elect_div,
-#          pop = Total_Population) %>%
-#   right_join(elect_map) -> elect_map
-# 
-# 
-# ggplot(elect_map) + geom_polygon(aes(long, lat, group = group),
-#                                  colour = 'grey')
-
-
-##Australian map
-#ausShp <- rgdal::readOGR('Aus')
-#ausSmall <- rmapshaper::ms_simplify(ausShp, keep = 0.05)
-#load("~/mapsR/ausSmall.Rda")
-# 
-# aus_data <- ausSmall@data
-# aus_data$id <- row.names(aus_data)
-# aus_map <- ggplot2::fortify(ausSmall)
-# aus_map$group <- paste("g", aus_map$group, sep = ".")
-# aus_map$piece <- paste("p", aus_map$piece, sep = ".")
-# 
-# # Switch states to the codes and attach population data to map
-# aus_data %>%
-#   mutate(State = case_when(
-#     STE_NAME16 == 'New South Wales' ~ 'NSW',
-#     STE_NAME16 == 'Victoria' ~ 'VIC',
-#     STE_NAME16 == 'Queensland' ~ 'QLD',
-#     STE_NAME16 == 'South Australia' ~ 'SA',
-#     STE_NAME16 == 'Western Australia' ~ 'WA',
-#     STE_NAME16 == 'Tasmania' ~ 'TAS',
-#     STE_NAME16 == 'Northern Territory' ~ 'NT',
-#     STE_NAME16 == 'Australian Capital Territory' ~ 'ACT',
-#     TRUE ~ as.character(STE_NAME16))) %>%
-#   select(id, State) %>%
-#   right_join(statePopulation) %>%
-#   right_join(aus_map) -> aus_map
+ggplot(LGA_map_2) + geom_polygon(aes(long, lat, group = group,fill=Value))
+ 
