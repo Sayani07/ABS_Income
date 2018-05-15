767b3f0fa889ec14c06234cd57b03a30930742e7
Sayani07 <Sayani.Gupta@monash.edu>
2018-05-15 17:09:37 +1000

    Plotting local Moran and Gstar and local significance map

--- R/LGA_map1.R
+++ R/LGA_map1.R
@@ -9,7 +9,7 @@ usethis::use_git()
 #The column "Total Household income" was split into Weekly and Annual Income in excel
 
 #Shapefile ESRI Data Link
-#http://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1270.0.55.003July%202016?OpenDocument
+#- http://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1270.0.55.003July%202016?OpenDocument
 #Name of the file (Zip) to download: Local Government Areas ASGS Ed 2016 Digital Boundaries in ESRI Shapefile Format 
 
 library(tidyverse)
@@ -38,6 +38,9 @@ IncomeData %>%
 IncomeData_r %>%
   group_by(LGA_2016) %>%
   summarise(NOP_WHI = sum(`Value`)) -> LGA_Income_r
+
+#summary(LGA_Income_r)
+#sort(LGA_Income_r$NOP_WHI)
 
 ggplot(IncomeData_r,aes(x=LGA_2016,y=Value))+geom_line()+ylab("# of households")+xlab("LGA")
 
@@ -85,8 +88,7 @@ LGA_data %>%
   right_join(IncomeData_r,by=c("LGA_CODE16" = "LGA_2016")) %>%
   right_join(LGA_map)-> LGA_map_2
 
-
-ggplot(LGA_map_2) + geom_polygon(aes(long, lat, group = group,fill=Value))
+ggplot(LGA_map_2) + geom_polygon(aes(long, lat, group = group,fill=Value)) + coord_fixed()
 
 #Creating k nearest Neighbours
 # knn2nb converts a knn object returned by knearneigh into a neighbours list of class nb with a list of integer vectors containing neighbour region number ids.
@@ -116,7 +118,7 @@ LGA_nn_1 <- make.sym.nb(LGA_nn_1)
 #corresponding to the neighbour object for first nearest neighbours. The
 #greatest value will be the minimum distance needed to make sure that all the
 #areas are linked to at least one neighbour
-dsts <- unlist(nbdists(LGA_nn_1, coords))
+dsts <- unlist(nbdists(LGA_nn_1, coords,longlat=TRUE))
 summary(dsts)
 max_1nn <- max(dsts)
 max_1nn
@@ -124,7 +126,7 @@ max_1nn
 #nb2listw function takes a neighbours list object and converts it into a weights object
 #For style="W", the weights vary between unity divided by the largest and
 #smallest numbers of neighbours, and the sums of weights for each areal entity
-#are unity
+#are unity  
 #B is the basic binary coding
 
 Weights_W <- nb2listw(LGA_nn_1,style="W")
@@ -133,96 +135,126 @@ Weights_C <- nb2listw(LGA_nn_1,style= "C")
 Weights_U <- nb2listw(LGA_nn_1,style="U")
 Weights_S <- nb2listw(LGA_nn_1,style="S")
 #If global linear spatial association exists
-moran.test(LGA_Income_r$NOP_WHI, Weights_W)
+Moran_test <-moran.test(LGA_Income_r$NOP_WHI, Weights_W)
 #Spatial dependence is there as p-value is very low
 #Moran I statistic       Expectation          Variance 
 #0.496028646      -0.001838235       0.002188406 
 #If correlation coefficient is close to 1, it means there is no variation betweeen data points and line of best fit
 #Here Moran's statistic is 0.49, implying there are large variation between data points and line of best fit. However, major variation
 #comes from the outliers.For most of the points, it seems to give a good fit. 
+#Global Geary Test
+Geary_test <-geary.test(LGA_Income_r$NOP_WHI, Weights_W,randomisation=TRUE)
+#Geary test suggests that there is some kind of non linear spatial association in the data - globally
 
 summary(moran.plot(LGA_Income_r$NOP_WHI, Weights_W))
-
-
+                                                                                                
+#Local measures of spatial autocorrelation
 local_Moran <- localmoran(LGA_Income_r$NOP_WHI, Weights_W)
 summary(local_Moran)
+
+
 #creating scaled resonse variable
 LGA_Income_r$NOP_WHI_s <- scale(LGA_Income_r$NOP_WHI)  %>% as.vector()
 # create a spatially lagged variable and save it to a new column
-LGA_Income_r$NOP_WHI_s_lag <- scale(lag.listw(Weights_W, LGA_Income_r$NOP_WHI))%>%as.vector()
+sLGA_Income_r$NOP_WHI_s_lag <- scale(lag.listw(Weights_W, LGA_Income_r$NOP_WHI))%>%as.vector()
 # summary of variables, to inform the analysis
 summary(LGA_Income_r$NOP_WHI_s)
 summary(LGA_Income_r$NOP_WHI_s_lag)
 
+Moran_MC<-moran.mc(LGA_Income_r$NOP_WHI, listw = Weights_W, nsim=10000)
+plot(Moran_MC)
+
+Geary_MC<-geary.mc(LGA_Income_r$NOP_WHI, listw = Weights_W, nsim=10000)
+plot(Geary_MC)
 
 
-LGA_Income_r$quad_sig <- NA
+LGA_Income_r$local_Moran_stat <- NA
 
 # high-high quadrant
 LGA_Income_r[(LGA_Income_r$NOP_WHI_s >= 0 & 
                 LGA_Income_r$NOP_WHI_s_lag >= 0) & 
-               (local_Moran[, 5] <= 0.05), "quad_sig"] <- "high-high"
+               (local_Moran[, 5] <= 0.05), "local_Moran_stat"] <- "high-high"
 # low-low quadrant
 
 LGA_Income_r[(LGA_Income_r$NOP_WHI_s <= 0 & 
                 LGA_Income_r$NOP_WHI_s_lag <= 0) & 
-               (local_Moran[, 5] <= 0.05), "quad_sig"] <- "low-low"
+               (local_Moran[, 5] <= 0.05), "local_Moran_stat"] <- "low-low"
 
 # high-low quadrant
 
 LGA_Income_r[(LGA_Income_r$NOP_WHI_s >= 0 & 
                 LGA_Income_r$NOP_WHI_s_lag <= 0) & 
-               (local_Moran[, 5] <= 0.05), "quad_sig"] <- "high-low"
+               (local_Moran[, 5] <= 0.05), "local_Moran_stat"] <- "high-low"
 
 # low-high quadrant
 
 LGA_Income_r[(LGA_Income_r$NOP_WHI_s <= 0 & 
                 LGA_Income_r$NOP_WHI_s_lag >= 0) & 
-               (local_Moran[, 5] <= 0.05), "quad_sig"] <- "low-high"
+               (local_Moran[, 5] <= 0.05), "local_Moran_stat"] <- "low-high"
 
 
 # non-significant observations
-LGA_Income_r[(local_Moran[, 5] > 0.05), "quad_sig"] <- "not signif."  
+LGA_Income_r[(local_Moran[, 5] > 0.05), "local_Moran_stat"] <- "not signif."  
 
 
 LGA_Income_r %>% 
-  group_by(quad_sig) %>% 
+  group_by(local_Moran_stat) %>% 
   tally()
 
-df <- fortify(LGA_Income_r, LGA_2016="id")
-df <- left_join(df, oregon.tract@data)
-df %>% 
-  ggplot(aes(long, lat, group = group, fill = quad_sig)) + 
-  geom_polygon(color = "white", size = .05)  + coord_equal() + 
-  theme_void() + scale_fill_brewer( palette = "Set1")
 
-
-# plotting the map
+# plotting the map for local_Morans
 df <- fortify(LGA_Income_r, region="LGA_2016")
-df1 <- left_join(df, LGA_data,by=c("LGA_2016"="LGA_CODE16"))
-df1 %>% 
-  ggplot(aes(long, lat, group = group, fill = quad_sig)) + 
-  geom_polygon(color = "white", size = .05)  + coord_equal() + 
-  theme_void() + scale_fill_brewer( palette = "Set1")
-
-
 df %>% right_join(IncomeData_r) %>%right_join(LGA_data,by=c("LGA_2016"="LGA_CODE16")) %>% right_join(LGA_map)-> LGA_map_3
 
 LGA_map_3 %>% 
-    ggplot(aes(long, lat, group = group, fill = quad_sig)) + 
+    ggplot(aes(long, lat, group = group, fill = local_Moran_stat)) + 
     geom_polygon(color = "white", size = .05)  + coord_equal() + 
-    theme_void() + scale_fill_brewer( palette = "Set1")
+    theme_void() + scale_fill_brewer( palette = "Set1") +coord_fixed()
 
-names(Weights_W)
-names(attributes(Weights_W))
-1/rev(range(card(Weights_W$neighbours)))
-summary(unlist(Weights_W$weights))
-set.seed(987654)
-n <- length(LGA_nn_1)
-uncorr_x <- rnorm(n)
-rho <- 0.5
-autocorr_x <- invIrW(Weights_W, rho) %*% uncorr_x
 
+#G and Gstar local spatial statistics
+local_G <-localG(LGA_Income_r$NOP_WHI, Weights_W)
+summary(local_G)
+
+local_G_new<- NA
+
+local_G_new[local_G >= 1.96] <- "high-high"
+local_G_new[(local_G <= -1.96)] <- "low-low"
+local_G_new[(-1.96<local_G)& (local_G<1.96)] <- "not_signif."
+
+LGA_Income_r2=cbind(LGA_Income_r,local_G_new)
+
+
+# plotting the map
+df <- fortify(LGA_Income_r2, region="LGA_2016")
+
+df %>% right_join(IncomeData_r) %>%right_join(LGA_data,by=c("LGA_2016"="LGA_CODE16")) %>% right_join(LGA_map)-> LGA_map_4
+
+LGA_map_4 %>% 
+  ggplot(aes(long, lat, group = group, fill = local_G_new)) + 
+  geom_polygon(color = "white", size = .05)  + coord_equal() + 
+  theme_void() + scale_fill_brewer( palette = "Set1")+ coord_fixed()
+
+
+#LGA_map_4 %>% filter(local_Moran_stat == c(""high-high","low-low"))
+##LGA where local Moran stat is significant and high high
+LGA_map_4 %>% 
+  group_by(local_Moran_stat,Region,STE_NAME16) %>% filter(local_Moran_stat=="high-high") %>% tally() ->Moran_High_High
+
+LGA_map_4 %>% 
+  group_by(local_Moran_stat,Region,STE_NAME16) %>% filter(local_Moran_stat=="low-low")%>%tally()->Moran_low_low
+##LGA where local  G stat is significant and high high
+LGA_map_4 %>% 
+  group_by(local_G_new,Region,STE_NAME16) %>% filter(local_G_new=="high-high")%>%tally()->G_High_High
+
+
+LGA_map_4 %>% 
+  group_by(local_G_new,Region,STE_NAME16) %>% filter(local_G_new=="low-low") %>%tally()->G_low_low
+
+#spatially significant in local Moran but not in local Gi
+D1 = setdiff(Moran_High_High$Region,G_High_High$Region)
+#spatially significant in local Gi but not in local Moran 
+D2 = setdiff(G_High_High$Region,Moran_High_High$Region)
 
 ##Done till here
 ##With LGA_map_2
@@ -253,7 +285,11 @@ LGA_nn_1 <- make.sym.nb(LGA_nn_1)
 #corresponding to the neighbour object for first nearest neighbours. The
 #greatest value will be the minimum distance needed to make sure that all the
 #areas are linked to at least one neighbour
-dsts <- unlist(nbdists(LGA_nn_1, coords))
+#Great Circle distances 
+dsts <- unlist(nbdists(LGA_nn_1, coords,longlat = TRUE))
+# Euclidean distances 
+dsts <- unlist(nbdists(LGA_nn_1, coords,longlat = TRUE))
+
 summary(dsts)
 max_1nn <- max(dsts)
 max_1nn
@@ -264,24 +300,5 @@ max_1nn
 #are unity
 #B is the basic binary coding
 
-Weights_W <- nb2listw(LGA_nn_1,style="W")
-Weights_B <- nb2listw(LGA_nn_1,style="B")
-Weights_C <- nb2listw(LGA_nn_1,style= "C")
-Weights_U <- nb2listw(LGA_nn_1,style="U")
-Weights_S <- nb2listw(LGA_nn_1,style="S")
-
-names(Weights_W)
-names(attributes(Weights_W))
-1/rev(range(card(Weights_W$neighbours)))
-summary(unlist(Sy0_lw_W$weights))
-set.seed(987654)   
-n <- length(LGA_nn_1)
-uncorr_x <- rnorm(n)
-rho <- 0.5
-autocorr_x <- invIrW(Weights_W, rho) %*% uncorr_x
 
 
-
-
-lmoran <- localmoran(oregon.tract$white, listw)
-summary(lmoran)
